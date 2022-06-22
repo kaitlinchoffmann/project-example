19922225f6823b812024c4c79d978d503fd3c1ab
kaitlinhoffmann <kaitlinchoffmann@gmail.com>
2022-06-22 04:07:38 -0400

    Setting Up React

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 // 1. import mongoose
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 
 // 2. create schema for entity
 const userSchema = new mongoose.Schema({
@@ -18,21 +19,27 @@ async function register(username, password) {
   const user = await getUser(username);
   if(user) throw Error('Username already in use');
 
+  const salt = await bcrypt.genSalt(10);
+  const hashed = await bcrypt.hash(password, salt);
+
   const newUser = await User.create({
     username: username,
-    password: password
+    password: hashed
   });
 
-  return newUser;
+  return newUser._doc;
 }
 
 // READ a user
 async function login(username, password) {
   const user = await getUser(username);
   if(!user) throw Error('User not found');
-  if(user.password != password) throw Error('Wrong Password');
 
-  return user;
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if(!isMatch) throw Error('Wrong Password');
+
+  return user._doc;
 }
 
 // UPDATE
